45ea5253eab20a27dbe2c40be68bdab64b26b351
JiY <jiyong3954@gmail.com>
2025-01-11 22:23:43 +0900

    fix : `:data:_source:local:room` / SampleEntity / @PrimaryKey 추가

--- data/_source/local/room/src/main/java/com/dd2d/data/source/local/room/entity/SampleEntity.kt
+++ data/_source/local/room/src/main/java/com/dd2d/data/source/local/room/entity/SampleEntity.kt
@@ -1,9 +1,10 @@
 package com.dd2d.data.source.local.room.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity("sample_table")
 data class SampleEntity(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val data: String,
 )
